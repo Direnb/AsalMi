d8131d271cc42148d61b98cb7ab4dac33bec9b00
Direnb <44303178+Direnb@users.noreply.github.com>
2020-07-13 20:12:17 +0300

    Add files via upload

---
+++ Uyg-Asal.py
@@ -0,0 +1,23 @@
+'''
+Soru : Girilen bir sayınn asal olup olmadığını bulunuz.
+**Asal sayı 1 ve kendisi hariç tam böleni olmayan sayılara denir.
+
+
+'''
+
+sayi = int(input('sayi: '))
+asalmi = True
+if sayi == 1:
+	asalmi = False
+for i in range(2,sayi):
+	if (sayi% i == 0):
+		asalmi = False
+		break
+
+if asalmi:
+	print('sayi asaldir')
+else:
+	print('sayi asal değildir')
+
+
+
